bda289b0aea7be39c73066b993ce9e4bf9df7d54
Keith Packard <keithp@keithp.com>
2008-07-29 11:58:16 -0700

    Reset lastSlave when disabling devices

--- dix/devices.c
+++ dix/devices.c
@@ -315,6 +315,14 @@ DisableDevice(DeviceIntPtr dev)
                 AttachDevice(NULL, other, NULL);
         }
     }
+    else
+    {
+        for (other = inputInfo.devices; other; other = other->next)
+        {
+	    if (other->isMaster && other->u.lastSlave == dev)
+		other->u.lastSlave = NULL;
+	}
+    }
 
     if (dev->isMaster && dev->spriteInfo->sprite)
     {
