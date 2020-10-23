987e88b2c3fe29d8bc4f1618d2339294eca51281
Michel Dänzer <mdaenzer@redhat.com>
2020-10-23 12:25:16 +0200

    meson: Change project name to xwayland
    (cherry picked from commit 001f0c89383a2c5227175da1fff8c9592413ba4c)

--- .gitlab-ci/dist.sh
+++ .gitlab-ci/dist.sh
@@ -7,8 +7,8 @@ meson build/
 meson dist --no-tests -C build/
 
 cd build
-tar xf meson-dist/xserver-*.tar.xz
-cd xserver-*/
+tar xf meson-dist/xwayland-*.tar.xz
+cd xwayland-*/
 
 meson -Dc_args="-fno-common" -Dprefix=/usr -Dwerror=true build/
 DESTDIR=$PWD/install/ ninja -j${FDO_CI_CONCURRENT:-4} -C build/ install
